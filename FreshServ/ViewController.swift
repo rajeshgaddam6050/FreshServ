ffbd7222ff9e6c0193bf10ce8320458e9cc5307b
IOSUSER <iosuser@IOSUSERs-iMac.local>
2018-01-02 10:46:00 +0530

    Initial Commit

---
+++ FreshServ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreshServ
+//
+//  Created by IOSUSER on 1/2/18.
+//  Copyright © 2018 IOSUSER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
